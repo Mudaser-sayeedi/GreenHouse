bf21b21d9681fc11eda0aa34eb0a47b9ee590629
mudaser sayeedi <mudasersayeed@gmail.com>
2022-07-16 20:07:49 +0430

    deleted unnecessary files and codes!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
+import { Skeleton } from '@mui/material';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Skeleton variant="text" />
+      <Skeleton variant="circular" width={40} height={40} />
+      <Skeleton variant="rectangular" width={210} height={118} />
     </div>
   );
 }
